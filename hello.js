4c666f285812d070cd22ef292c31366a7896b848
Ganesh Manda <104066587+MandaGanesh@users.noreply.github.com>
2022-07-07 12:55:57 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> hello world program js</title>
+</head>
+<body>
+<p> hello world program </p>
+<script>
+alert('Hello World');
+</script>
+<p> hello</p>
+</body>
+</html>
